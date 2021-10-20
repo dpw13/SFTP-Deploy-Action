d97cbffaac4161bd1fe92bbb5362b8edcccdc9ea
Lubricant Jam <LubricantJam@users.noreply.github.com>
2021-10-20 21:31:19 +0100

    Attempt at fixing SSH Command.

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,7 +45,9 @@ then
     echo 'No SSH command specified, success!'
 else
     echo 'SSH command being ran...'
-    sshpass -p $4 ssh -o StrictHostKeyChecking=no $1@$2:$3 "cd $6;$8"
+sshpass -p $4 sftp -P $3 $7 -o StrictHostKeyChecking=no $1@$2 <<-EOF
+cd $6;$8
+EOF
     echo 'SSH command completed!'
 fi
 
