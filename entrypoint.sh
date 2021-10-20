983c266440e1e483e0db184807382db368be92d0
James Finch <LubricantJam@users.noreply.github.com>
2021-10-20 21:15:22 +0100

    Added File Compression & SSH Command. (#1)
    Added:
    - The ability to compress files upon upload, this will not extract also however you can use the ``ssh_command`` parameter to manipulate this archive as you feel.
    - The ability to specify a SSH command upon transfer.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,9 @@
 #set -e at the top of your script will make the script exit with an error whenever an error occurs (and is not explicitly handled)
 set -eu
 
-TEMP_SFTP_FILE='../sftp'
+TSTAMP=$(date -I)
+TEMP_SFTP_FILE="../sftp"
+ARCHIVE_FILE="../archive-$TSTAMP.tar.gz"
 
 echo 'Connecting to SSH server and creating directory..'
 
@@ -12,10 +14,29 @@ sshpass -p "$4" ssh -o StrictHostKeyChecking=no -p $3 $1@$2 mkdir -p $6
 echo 'Connection to SSH server and directory creation finished successfully!'
 
 echo 'Starting file transfer..'
-# create a temporary file containing sftp commands
-printf "%s" "put -r $5 $6" >$TEMP_SFTP_FILE
-#-o StrictHostKeyChecking=no to avoid "Host key verification failed".
-sshpass -p "$4" sftp -oBatchMode=no -b $TEMP_SFTP_FILE -P $3 $7 -o StrictHostKeyChecking=no $1@$2
+if $9
+then
+    echo 'Starting compression...'
+    tar -czvf $ARCHIVE_FILE $5
+    echo 'Finishing compression...'
+# Formatting can't be resolved, feel free to PR if I am wrong.
+sshpass -p $4 sftp -P $3 $7 -o StrictHostKeyChecking=no $1@$2 <<-EOF
+put -r $ARCHIVE_FILE $6
+EOF
+else
+    # create a temporary file containing sftp commands
+    touch $TEMP_SFTP_FILE
+    if [ -d "$5" ]
+    then
+    printf "%s\n" "-mkdir $6" >$TEMP_SFTP_FILE
+    (cd $5; find * -type d -exec echo -mkdir $6/{} \;) >>$TEMP_SFTP_FILE
+    printf "%s" "put -r $5* $6" >>$TEMP_SFTP_FILE
+    else
+    printf "%s" "put $5 $6" >>$TEMP_SFTP_FILE
+    fi
+    #-o StrictHostKeyChecking=no to avoid "Host key verification failed".
+    sshpass -p "$4" sftp -oBatchMode=no -b $TEMP_SFTP_FILE -P $3 $7 -o StrictHostKeyChecking=no $1@$2
+fi
 
 echo 'File transfer finished successfully!'
 
